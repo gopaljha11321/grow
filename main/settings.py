8224cbd47b97ec6f85e328cec40a6e5d30ec1615
gopaljha11321 <gopaljha11321@gmail.com>
2022-07-10 20:51:09 +0530

    test2

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%f5^zq2p5gn!@tch!sx%i^&epv=ghjvj$0&l#3lzp!q%$@*^ee
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',"app-grow1.herokuapp.com"]
+ALLOWED_HOSTS = ["grow-project.herokuapp.com","127.0.0.1"]
 
 
 # Application definition
--- main/urls.py
+++ main/urls.py
@@ -18,12 +18,10 @@ from django.urls import path,include
 # from . import urls
 from . import views
 from django.conf import settings
-from django.conf.urls.static import static,serve,url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("intro.urls")),
     path('project/',include("circuitG.urls")),
     path('teach/',include("teach.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
