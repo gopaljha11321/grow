93c3fe4204538c264af9916a1584c1bed075569d
gopaljha11321 <gopaljha11321@gmail.com>
2022-08-28 11:01:27 +0530

    second commit

--- main/settings.py
+++ main/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,17 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL='static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL='/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
     ]
-
-
-# STATIC_ROOT = os.path.join(BASE_DIR,'public')
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
